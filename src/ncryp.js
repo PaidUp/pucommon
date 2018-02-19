49ad2c45a860dd040fcbae40308e3c2e7157305b
Ricardo Lara <riclara@gmail.com>
2018-02-19 10:01:23 -0300

    fix ncryp

--- src/ncryp.js
+++ src/ncryp.js
@@ -1,17 +1,19 @@
 import Blind from 'blind'
 
+let encryptKey
+
 export default class Ncryp {
   set key (key) {
-    this.encryptKey = key
+    encryptKey = key
   }
 
   static encryptField (value) {
-    let encrypted = new Blind({ encryptKey: this.encryptKey }).encrypt(value)
+    let encrypted = new Blind({ encryptKey }).encrypt(value)
     return encrypted
   }
 
   static decryptField (encryptedValue) {
-    var decrypted = new Blind({ encryptKey: this.encryptKey }).decrypt(encryptedValue)
+    var decrypted = new Blind({ encryptKey }).decrypt(encryptedValue)
     return decrypted
   }
 }
