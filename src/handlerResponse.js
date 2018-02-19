aa64e18515cb10bbeca84449156a14370a4e8fe9
Ricardo Lara <riclara@gmail.com>
2018-02-18 22:54:45 -0300

    fix handle response

--- src/handlerResponse.js
+++ src/handlerResponse.js
@@ -2,11 +2,11 @@ import Logger from './logger'
 
 export default class HandlerResponse {
   static send (response, msg) {
-    return this.response.status(200).json(msg)
+    return response.status(200).json(msg)
   }
 
   static error (response, msg, code) {
     Logger.error({code: code, resason: msg})
-    return this.response.status(code || 500).json(msg)
+    return response.status(code || 500).json(msg)
   }
 }
