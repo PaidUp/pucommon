ccdb689860f0e258739eaf1f756a1ae8cccb14b1
Ricardo Lara <riclara@gmail.com>
2018-02-18 23:55:03 -0300

    add stripe module

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import Ncryp from './ncryp'
 import HandlerResponse from './handlerResponse'
 import Auth from './auth'
 import Calculations from './calculations'
+import Stripe from './stripe'
 
 export {
   Logger,
   Ncryp,
   HandlerResponse,
   Auth,
-  Calculations
+  Calculations,
+  Stripe
 }
