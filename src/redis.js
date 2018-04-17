1a908d7e53729efcc52086954ce8b163fa8a226a
Ricardo Lara <riclara@gmail.com>
2018-04-16 19:58:36 -0500

    implement redis reconnect

--- src/redis.js
+++ src/redis.js
@@ -2,7 +2,28 @@ import redis from 'redis'
 
 export default class Redis {
   constructor (port, host) {
-    this.client = redis.createClient(port, host)
+    this.client = redis.createClient({
+      port,
+      host,
+      retry_strategy: function (options) {
+        if (options.error && options.error.code === 'ECONNREFUSED') {
+          // End reconnecting on a specific error and flush all commands with
+          // a individual error
+          return new Error('The server refused the connection')
+        }
+        if (options.total_retry_time > 1000 * 60 * 60) {
+          // End reconnecting after a specific timeout and flush all commands
+          // with a individual error
+          return new Error('Retry time exhausted')
+        }
+        if (options.attempt > 10) {
+          // End reconnecting with built in error
+          return undefined
+        }
+        // reconnect after
+        return Math.min(options.attempt * 100, 3000)
+      }
+    })
     this.client.on('connect', function () {
     })
   }
