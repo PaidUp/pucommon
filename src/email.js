7ceb33ada577f290fee44d4d68146bdbcfa49a8a
Ricardo Lara <riclara@gmail.com>
2018-08-06 14:34:47 -0500

    add from required to email

--- src/email.js
+++ src/email.js
@@ -1,13 +1,19 @@
 import sgMail from '@sendgrid/mail'
 
 export default class Email {
-  constructor (apiKey) {
+  constructor (apiKey, fromName, fromEmail) {
+    this.fromName = fromName
+    this.fromEmail = fromEmail
     sgMail.setApiKey(apiKey)
   }
 
   sendTemplate (to, templateId, substitutions) {
     const msg = {
       to,
+      from: {
+        email: this.fromEmail,
+        name: this.name
+      },
       template_id: templateId,
       substitutions
     }
